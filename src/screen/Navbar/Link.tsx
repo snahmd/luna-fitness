5ea1a9498aeef74d310bcb9d14b1a1a4cd6d3011
snahmd <sanahmed7@gmail.com>
2024-07-26 23:26:59 +0200

    add navbar link in shared folder

---
+++ src/screen/Navbar/Link.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SelectedPage } from "../../shared/types";
+
+type Props = {};
+
+const Link = (props: Props) => {
+  return <div>Link</div>;
+};
+
+export default Link;
