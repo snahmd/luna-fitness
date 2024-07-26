48554d24c2c058e5086ed82373eb82fbc0214fb6
snahmd <sanahmed7@gmail.com>
2024-07-27 00:52:32 +0200

    create hooks for mediaQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./screen/Navbar";
 
 function App() {
+  // create selectedPage state with type SelectedPage and setSelectedPage function
+  const [selectedPage, setSelectedPage] = useState("Home");
   return (
     <>
       <div>
-        <Navbar></Navbar>
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       </div>
     </>
   );
--- src/screen/Navbar/index.tsx
+++ src/screen/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import Logo from "../../assets/logo.png";
 import Link from "./Link";
 import { SelectedPage } from "../../shared/types";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -9,35 +11,42 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
     <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
       <div className={`${flexBetween} mx-auto w-5/6`}>
         <div className={`${flexBetween} w-full gap-16`}>
           <img src={Logo} alt="logo" className="" />
-          <div className={`${flexBetween}w-full`}>
-            <div className={`${flexBetween}mt-4 gap-8 text-sm`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              ></Link>
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              ></Link>
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              ></Link>
-              <Link
-                page="Contact"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              ></Link>
+          {isAboveMediumScreens ? (
+            <div className={`${flexBetween}w-full`}>
+              <div className={`${flexBetween}mt-4 gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                ></Link>
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                ></Link>
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                ></Link>
+                <Link
+                  page="Contact"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                ></Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            <button className="rounded-full bg-secondary-500 p-2">
+              <Bars3Icon className="h-6 w-6 text-black" />
+            </button>
+          )}
         </div>
       </div>
     </div>
